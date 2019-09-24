5679b8b22dab1c729f067aacdee609ffd2d5cfb8
Pawel <paul@pawel.RF511>
2019-09-24 12:24:05 +0200

    Writing test for user invitation

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::group(['middleware'=>'auth'], function (){
 
     Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
 
+    Route::post('/projects/{project}/invitations', 'ProjectInvitationsController@store');
+
     Route::get('/home', 'HomeController@index')->name('home');
 
 });
